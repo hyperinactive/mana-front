dec52893687454fd7453aeb05315cb69493d8d9e
hyperinactive <hyperinactive.box@gmail.com>
2021-01-05 00:21:11 +0100

    Components update
    - new component SidebarChat
    - style changes to components: App, Chat, Sidebar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
   return (
     <div className="app">
+    <h1>MANA</h1>
       <div className="app__body">
-        <h1>MANA</h1>
 
         <Sidebar />
         <Chat />
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,49 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat/SidebarChat";
+
 
 function Sidebar() {
   return (
-    <div>
-      <h1>I be the sidebar thingy</h1>
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+
+        <div className="sidebar__headerRight">
+          {/* icon button makes it clickable */}
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+
+        </div>
+      </div>
+      
+      <div className="sidebar__search">
+          <div className="sidebar__searchContainer">
+            <SearchOutlinedIcon />
+            <input type="text" placeholder="Search or start a new chat"/>
+          </div>
+      </div>
+
+      <div className="sidebar__chat">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+      
     </div>
   );
 }
